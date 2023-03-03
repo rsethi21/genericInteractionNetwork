864d76b9e228ac81c0678a506f720ab36c0a6733
Rohan Sethi <rsethi1@luc.edu>
2023-03-03 13:01:52 -0600

    fixed more circular imports

--- openIt.py
+++ openIt.py
@@ -1,14 +1,4 @@
-from networkObj import Network
-import argparse
-
-parser = argparse.ArgumentParser(description="open a network object stored in pickle format")
-parser.add_argument('-i', '--input', help='path to pickle file', required=True)
-
 def openNetwork(path):
     n = Network.readNetworkObject(path)
     return n
-
-if __name__ == '__main__':
-    args = parser.parse_args()
-    n = Network.readNetworkObject(args.input)
     
