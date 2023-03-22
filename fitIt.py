65256a88b4354061a9d1de5d1960ebbbc54ab08e
Rohan Sethi <rsethi1@luc.edu>
2023-03-21 22:23:25 -0500

    added population initialization, adjust and cost function to GeneticAlgo

--- fitIt.py
+++ fitIt.py
@@ -4,13 +4,14 @@ import argparse
 from tqdm import tqdm
 import openIt
 import modifyIt
-import odeint
+from scipy.integrate import odeint
 
 
 class geneticAlgo:
     def __init__(self, network, data, time, crossRate=0.8, mutRate=0.1, numGens=10, numInds=10, constraints=None):
         self.network = network
         self.data = data
+        self.time = time
         self.ratesDict = {}
         self.crossRate = crossRate
         self.mutRate = mutRate
@@ -19,7 +20,7 @@ class geneticAlgo:
         self.initialpopulation = []
         self.constraints = constraints
 
-    def intialPopulation(self):
+    def initialPopulation(self):
         ratesDictionary = {}
         for s in self.network.substrates:
             k = s.phosRate
@@ -52,20 +53,40 @@ class geneticAlgo:
         keys = list(self.ratesDict.keys())
         for i, r in enumerate(rates):
             # need some way to identify what rate each index applies to
+            # need to change indices from one to looking for the spaces for adaptability to longer names
             key = keys[i]
-            self.ratesDict[]
+            if key[0] == 'k':
+                subs = key[-1]
+                for s in self.network.substrates:
+                    if subs == s.name:
+                        s.phosRate = r
+            elif key[0] == 'r':
+                subs = key[-1]
+                for s in self.network.substrates:
+                    if subs == s.name:
+                        s.dephosRate = r
+            else:
+                sub1 = key[0]
+                sub2 = key[3]
+                for i in self.network.interactions:
+                    if i.substrate1 == sub1 and i.substrate2 == sub2:
+                        i.rate = r
+
+        y0 = self.network.getInitialValues()
+        y = odeint(self.network.diffEQs, y0, self.time) 
+
         return y
 
     def cost(self, new_y):
         metadata = [s.name for s in self.network.substrates]
-        modifyIt.editSubstrate(self.network)
         rss = 0
-        for key, value in self.data:
-            index = metadata.find(key)
-            time = value[0]
-            quant = value[1]
-            cost = (quant - new_y[time, index])**2
-            rss += cost
+        for key, value in self.data.items():
+            index = metadata.index(key)
+            for tup in value:
+                time = tup[0]
+                quant = tup[1]
+                cost = (quant - new_y[time, index])**2
+                rss += cost
         return rss
 
     def selection():
@@ -83,5 +104,7 @@ class geneticAlgo:
     def goodnessOfFit():
         return
 
-    def run():
-        return
+    def run(self):
+        self.initialPopulation()
+        y = self.adjustModel(self.initialpopulation)
+        return self.cost(y)
